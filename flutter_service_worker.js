c6c63dcb4563938714911ab8438a7ee0cdb4239a
ahmadadlan11 <a7madadlan@gmail.com>
2020-06-12 17:03:55 +0200

    Complete 4 steps2

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -12,7 +12,7 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "index.html": "8ca0198b60cee9cb5692837665befbdd",
 "/": "8ca0198b60cee9cb5692837665befbdd",
-"main.dart.js": "f65a2a9b1a952697f47f9d19c2ac23ca",
+"main.dart.js": "f19cb2a7758cdaa5446dcf86412f2eb4",
 "manifest.json": "954a5e8265276cc671b8edf7f4b2f741"
 };
 
